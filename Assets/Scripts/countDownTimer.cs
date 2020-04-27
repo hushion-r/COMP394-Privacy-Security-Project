65f38b5ed72ff65b5f8d0f28e2ddac49e68c0324
LINGAP <lma1169949842@gmail.com>
2020-04-27 02:26:04 -0500

    timer implemented

--- Assets/Scripts/countDownTimer.cs
+++ Assets/Scripts/countDownTimer.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class countDownTimer : MonoBehaviour
 {
+    float currentTime = 0f;
+    float startingTime = 60f;
+    [SerializeField] GameObject stopScene;
+    [SerializeField] Text countDownText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!stopScene.activeInHierarchy) {
+            currentTime -= 1 * Time.deltaTime;
+            countDownText.text = currentTime.ToString("0");
+        }
+
         
     }
 }
